a90e99a564a8434c5693dc29f10effd04e5c7724
KarunyaRam <sprkarunya986@gmail.com>
2026-05-24 08:43:11 +0530

    Selection sort Implementation

---
+++ Sorting/s1_selection-sort.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(vector<int> &v){
+int mini=0;
+int n=v.size();
+for(int i=0;i<n-2;i++){
+    mini=i;
+    for(int j=i;j<n-1;j++){
+        if(v[j]<v[mini]){
+            mini=j;
+        }
+    }
+    swap(v[i],v[mini]);
+}
+}
+
+int main(){
+    vector<int> v;
+    v={2,7,4,1,9};
+    selection_sort(v);
+    // int n=v.size();
+    // int mini;
+    // for(int i=0;i<n-2;i++){
+    //     mini=i;
+    //     for(int j=i;j<n-1;j++){
+    //         if(v[j]<v[mini]){
+    //             mini=j;
+    //         }
+    //     }
+    //     swap(v[i],v[mini]);
+
+    // }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+
+}
