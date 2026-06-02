378446eeb6f4316a97391f56e8b7d4a8bc973e72
KarunyaRam <sprkarunya986@gmail.com>
2026-06-02 14:49:38 +0530

    Left Rotate Array by One Place

---
+++ Arrays/LeftRotate/s1_byoneplace.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v={1,2,3,4,5};
+    int temp=v[0];
+    for(int i=1;i<v.size();i++){
+        v[i-1]=v[i];
+    }
+    v[v.size()-1]=temp;
+
+    for(auto it:v){
+        cout<<"The Array elements are: "<<it<<endl;
+    }
+}
